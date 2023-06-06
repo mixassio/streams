cf9b0a2d1b7be0edeef7b4aed0d2ffa6a1a3f31b
Mikhail Bondarev <mikhail.bondarev@onetwotrip.com>
2023-06-06 11:50:30 +0200

    init

---
+++ stream1.ts
@@ -0,0 +1,33 @@
+import { createReadStream, createWriteStream } from 'fs'
+
+const delimeter = '|';
+const newLine = '\n';
+
+const readStream = createReadStream('data1.csv', { highWaterMark: 1 });
+const writeStream = createWriteStream('data_result.csv');
+const bufferField: string[] = [];
+const bufferLine: string[] = [];
+let isHeaders = true;
+
+readStream.on('data', (chunk: string | Buffer) => {
+  const element = chunk.toString()
+  if (element === delimeter) {
+    bufferLine.push(bufferField.join(''))
+    bufferField.length = 0;
+  } else if (element !== newLine) {
+    bufferField.push(element);
+  } else {
+    bufferLine.push(bufferField.join(''))
+    if (isHeaders) {
+      bufferLine.push('length_name')
+      isHeaders = false;
+    } else {
+      bufferLine.push(bufferLine[0].length.toString())
+    }
+    console.log(bufferLine);
+    // как вернуть данные чтобы перешло в .pipe(writeStream) без следующей строчки?
+    writeStream.write(bufferLine.join('|') + '\n')
+    bufferField.length = 0;
+    bufferLine.length = 0
+  }
+}) // .pipe(writeStream)
---
+++ stream2.ts
@@ -0,0 +1,46 @@
+import { createReadStream, createWriteStream } from 'fs'
+import { Transform } from 'stream';
+
+const delimeter = '|';
+const newLine = '\n';
+
+const readStream = createReadStream('data1.csv', { highWaterMark: 1 });
+const writeStream = createWriteStream('data_result.csv');
+const bufferField: string[] = [];
+const bufferLine: string[] = [];
+let isHeaders = true;
+
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    const element = chunk.toString()
+    if (element === delimeter) {
+      bufferLine.push(bufferField.join(''))
+      bufferField.length = 0;
+    } else if (element !== newLine) {
+      bufferField.push(element);
+    } else {
+      bufferLine.push(bufferField.join(''))
+      if (isHeaders) {
+        bufferLine.push('length_name')
+        isHeaders = false;
+      } else {
+        bufferLine.push(bufferLine[0].length.toString())
+      }
+      // this.push(bufferLine.join('|') + '\n');
+      callback(null, bufferLine.join('|') + '\n');
+      bufferField.length = 0;
+      bufferLine.length = 0
+      return;
+    }
+
+    callback();
+  }
+});
+
+const transformStream2 = new Transform({
+  transform(chunk, encoding, callback) {
+    console.log(chunk.toString());
+    callback(null, chunk);
+  }})
+
+readStream.pipe(transformStream).pipe(transformStream2).pipe(writeStream);
